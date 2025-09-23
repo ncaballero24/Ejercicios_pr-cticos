e987b5b05cb7ef1fe506879ef619f7da45c20278
ncaballero24 <papadeiris2024@gmail.com>
2025-09-22 18:47:46 -0600

    Add files via upload

---
+++ AndroidStudio/Recycler/app/src/main/java/com/koss/recycler/examData.java
@@ -0,0 +1,14 @@
+package com.koss.recycler;
+
+public class examData {
+    String name;
+    String date;
+    String message;
+
+    examData(String name,String date,String message)
+    {
+        this.name = name;
+        this.date = date;
+        this.message = message;
+    }
+}
